344fb3a3b77941c6711365d99e9651904897dd5d
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2017-03-14 14:01:05 +0000

    apphead context processor

--- contexts.py
+++ contexts.py
@@ -1,6 +1,17 @@
+from django import tempalte
+
 def appname(request):
     return {'appname': request.resolver_match.app_name }
 
 def urlname(request):
     return {'urlname': request.resolver_match.url_name }
+
+def apphead(request):
+    head_template = request.resolver_match.url_name+'/extra_head.html'
+    try:
+        template.loader.get_template(head_template) 
+        return {'apphead' : head_template}
+    except template.TemplateDoesNotExist:
+        return {'apphead' : 'extra_head.html'} #fall back if no tempalte available
+
 
