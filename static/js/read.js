4703c66dc2643b956bd9cd78eac6aba35fc590e2
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2017-03-14 14:56:46 +0000

    I have pared read.js right down to a few shared features so that dashboard.js and library.js can develop separately as necessary

--- static/js/read.js
+++ static/js/read.js
@@ -1,22 +1,3 @@
-$(document).ready(function(){
-	$(".menu-toggle-wrapper").css("height",window.innerHeight+'px');
-	var sidebar_state = localStorage.getItem("sidebar_state");
-	if (sidebar_state === "in") {
-		$("#wrapper").toggleClass("toggled");
-		$(".read_nav_out").show();
-		$(".read_nav_in").hide();
-	} else {
-		$(".read_nav_in").show();
-		$(".read_nav_out").hide();
-	}
-
-	$('.subnav').affix({
-      		offset: {
-        	top: $('#ts-top-nav').height()
-      		}
-	}); 
-    	$('[data-toggle="popover"]').popover(); 
-});
 
 /* globals... */
 var data_cache = {};
@@ -24,195 +5,22 @@ var charts = {};
 console.log(window.location.pathname);
 //We strip off the ids and should have a useful app base that will work for any server context
 var appbase = window.location.pathname.replace(/\/\d+(|\/)/g, "");
-var serverbase = window.location.pathname.replace(/(|\/\w+)\/\d+(|\/)/g, "");
+//var serverbase = window.location.pathname.replace(/\/\w+\/\d+(|\/)/g, ""); //nb expects slash
+//our urls will be like this:
+// domain.com/serverbase/appname/id/id/id/id/action
+// remove from the back nothing after word that *should* == appname and anything after it (to get server base)
+var serverbase = window.location.pathname.replace(/\/\w+(|\/|\/\d.*)$/g, ""); 
 
 console.log("APPBASE: ",appbase);
 console.log("SERVERBASE: ",serverbase);
 
-
-
-$(document).ready(function(){
-	$("#id_language").on("change keyup", function () {
-		$(this).closest("form").submit();
-	});
-	//actions (always)
-	console.log("STATIC_URL: ",static_url);	
-	actions_table = init_actions_table();
-	init_date_inputs(actions_table);
-	init_actions_chart();
-	init_user_actions_chart();
-	init_top_users_chart();
-	init_top_collections_chart()
-
-	init_user_list();
-
-	init_collections_table();
-	init_users_table();
-	init_documents_table();
-//	init_pages_table();
-	init_pages_thumbs();
-
-	init_chart_filters();
-});
 function make_url(url){
-	appbase = appbase.replace(/\/$/,""); //remove trailing slash from appbase
-	return appbase+url;
-}
-function init_date_inputs(actions_table){
-
-    var min = new Date('2016.01.01').getTime() / 1000;
-    var max = new Date().getTime() / 1000;
-
-    var slider = $( "#slider-range" ).slider({
-      range: true,
-      min: min, //seconds
-      max: max,
-      step: 86400,
-      values: [ min, max ],
-      slide: function( event, ui ) {
-        $( "#start_date" ).val( (new Date(ui.values[ 0 ] *1000).toDateString() ) );
-        $( "#end_date" ).val( (new Date(ui.values[ 1 ] *1000)).toDateString() );
-      },
-      change: function(event, ui){
-	//if the slider values change we reload the data table...
-	actions_table.ajax.reload();
-      }
-    });
-    $( "#start_date" ).datepicker({"dateFormat":"D M dd yy", "onSelect": function(date){
-		console.log("start date picker onSelect");
-		slide_end = slider.slider("option", "values")[1];
-		slider.slider( "option", "values", [($(this).datepicker("getDate")/1000),slide_end] );
-	}}).val(new Date(min*1000).toDateString());
-    $( "#end_date" ).datepicker({"dateFormat":"D M dd yy","onSelect": function(date){
-		console.log("end date picker onSelect");
-		slide_start = slider.slider("option", "values")[0];
-		slider.slider( "option", "values", [slide_start,($(this).datepicker("getDate")/1000)] );
-	}}).val(new Date(max*1000).toDateString());
-
-}
-function init_actions_table(){
-
-	if(!$("#actions_table").length) return;
-
-	var ids = parse_path();
-	var url = make_url("/table_ajax/actions");
-	console.log("Using appbase: ",url);
-
-//	var url = "./table_ajax/actions";
-	var context = '';
-	for(x in ids){
-		console.log(x," => ",ids[x])
-		context += '/'+ids[x];
-	};
-	url += context;
-	console.log("URL: ",url);
-	var columns =  [
-		    { "data": "time" },
-		    { "data": "colId", "visible": false },
-		    { "data": "colName" },
-		    { "data": "docId", "visible": false  },
-		    { "data": "docName" },
-		    { "data": "pageId", "visible": false  },
-		    { "data": "pageNr" },
-		    { "data": "userName" },
-		    { "data": "type" }
-        	];
-	return init_datatable($("#actions_table"),url,columns);
-}
-
-function init_users_table(){
-
-	if(!$("#users_table").length) return;
-
-	var ids = parse_path();
-	var url = make_url("/table_ajax/users");
-
-	var context = '';
-	for(x in ids){
-		context += '/'+ids[x];
-	};
-	url += context;
-	var columns =  [
-		    { "data": "userId", "visible": false  },
-		    { "data": "userName"},
-		    { "data": "firstname" },
-		    { "data": "lastname" },
-		    { "data": "email" },
-		    { "data": "affiliation" },
-		    { "data": "created",
-		      "render" : function(data, type, row){
-				if(data === "n/a") return data;
-				var date = new Date(data)	
-				return date.toDateString();
-			},
-		    },
-		    { "data": "role" },
-        	];
-	return init_datatable($("#users_table"),url,columns);
-}
-function init_collections_table(){
-
-	if(!$("#collections_table").length) return;
-
-	var url = make_url("/table_ajax/collections");
-
-	var columns =  [
-		    { "data": "colId" },
-		    { "data": "colName",
-		      "render" : function(data, type, row){
-				return '<a href="'+row.colId+'">'+data+'</a>';
-			} 
-		    },
-		    { "data": "description" },
-		    { "data": "role" },
-        	];
-	init_datatable($("#collections_table"),url,columns);
-}
-
-function init_documents_table(){
-
-	if(!$("#documents_table").length) return;
-
-//	var url = "./table_ajax/documents/"+window.location.pathname.replace(/^.*\/(\d+)$/, '$1');
-	var url = make_url("/table_ajax/documents/"+window.location.pathname.replace(/^.*\/(\d+)$/, '$1'));
-
-	var ids = parse_path();	
-
-	var columns =  [
-		    { "data": "docId" },
-		    { "data": "title" },
-		    { "data": "author" },
-		    { "data": "uploadTimestamp",
-		      "render" : function(data, type, row){
-				var date = new Date(data)	
-				return date.toDateString();
-			} 
-		    },
-		    { "data": "uploader" },
-		    { "data": "nrOfPages" },
-		    { "data": "language" },
-		    { "data": "status" },
-        	];
-	init_datatable($("#documents_table"),url,columns);
-}
-
-function init_pages_table(){
-
-//	var url = "./table_ajax/pages"+window.location.pathname.replace(/^.*\/(\d+\/\d+)$/, '$1');
-	var url = make_url("/table_ajax/pages"+window.location.pathname.replace(/^.*\/(\d+\/\d+)$/, '$1'));
-
-
-
-	var ids = parse_path();	
-
-	var columns =  [
-		    { "data": "pageId" },
-		    { "data": "pageNr" },
-		    { "data": "thumbUrl" },
-		    { "data": "status" },
-		    { "data": "nrOfTranscripts" },
-        	];
-	init_datatable($("#pages_table"),url,columns);
+//	appbase = appbase.replace(/\/$/,""); //remove trailing slash from appbase
+//	return appbase+url;
+	//we will switch to using serverbase as we may need to call ajax views across the constituent apps
+	//NB this change means the app that the view is from must be specigied in the url
+	serverbase = serverbase.replace(/\/$/,""); //remove trailing slash from appbase
+	return serverbase+url;
 
 }
 
@@ -289,89 +97,6 @@ function init_datatable(table,url, columns){
 		$("#"+$(table).attr("id")+"_count").html(datatable.page.info().recordsTotal);
 	} );
 	return datatable;
-}
-
-
-function init_actions_chart(){
-
-	if(!$("#actions_line").length) return;
-	ids=parse_path();
-//	var url = static_url+"/dashboard/chart_ajax/actions/line";
-	var url = make_url("/chart_ajax/actions/line");
-
-	for(id in ids){
-		url += '/'+ids[id];
-	}
-	init_chart("actions_line",url,"line");
-
-}
-
-//TODO propogate / better integrate this
-function init_user_actions_chart(userid,canvas_id){
-
-	if(canvas_id == undefined) canvas_id = "user_actions_line";
-	if(!$("#"+canvas_id).length) return;
-	ids=parse_path();
-	//userid is set in header template and is the current_userid of currently logged in user
-//	console.log("USERNAME: ",current_userid);
-	if(userid == undefined) userid = current_userid;
-//	var url = static_url+"/dashboard/chart_ajax/u/"+userid+"/actions/line";
-	var url = make_url("/chart_ajax/u/"+userid+"/actions/line");
-
-	for(id in ids){
-		url += '/'+ids[id];
-	}
-	init_chart(canvas_id,url,"line");
-}
-
-function init_top_users_chart(){
-
-	if(!$("#top_users").length) return;
-	ids=parse_path();
-//	var url = static_url+"/dashboard/chart_ajax/actions/top_bar/userId/userName";
-	var url = make_url("/chart_ajax/actions/top_bar/userId/userName");
-
-	for(id in ids){
-		url += '/'+ids[id];
-	}
-	init_chart("top_users",url,"bar");
-}
-
-function init_top_collections_chart(){
-
-	if(!$("#top_collections").length) return;
-	ids=parse_path();
-//	var url = static_url+"/dashboard/chart_ajax/actions/top_bar/colId/colName";
-	var url = make_url("/chart_ajax/actions/top_bar/colId/colName");
-
-	init_chart("top_collections",url,"bar");
-}
-
-function init_doughnut(){
-
-	if(!$("#status_doughnut").length) return;
-	ids=parse_path();
-//	var url = static_url+"/dashboard/chart_ajax/actions/top_bar/userId/userName";
-	var url = make_url("/chart_ajax/status/doughnut");
-
-	for(id in ids){
-		url += '/'+ids[id];
-	}
-	init_chart("top_users",url,"doughnut");
-}
-
-
-
-function init_user_list(){
-	if(!$("#user_list").length) return;
-	ids=parse_path();
-//	var url = static_url+"/dashboard/table_ajax/users"; //use table data with -1... *nascent* javascript caching... will need to use url+paramss to stop pollution
-	var url = make_url("/table_ajax/users"); //use table data with -1... *nascent* javascript caching... will need to use url+paramss to stop pollution
-
-	for(id in ids){
-		url += '/'+ids[id];
-	}
-	init_list("user_list",url);
 }
 
 function init_list(list_id,url){
@@ -469,37 +194,7 @@ function init_chart(canvas_id,url,chart_type){
 	});
 
 }	
-function init_chart_filters(){
 
-	$(".table_filter").on("click", function(){
-		active_canvas = $(".tab-pane.active > canvas").attr("id");
-		console.log("table_filter CLICKED",active_canvas);
-
-		if($("#"+active_canvas).length==0) return false;
-
-		var chart =  charts[active_canvas];
-		//TODO the hiding needs to be inversed to make click on the button positive (ie just show clicked, rather than hide clicked)
-		for(x in chart.data.datasets){
-			var a=chart.getDatasetMeta(x);
-			a.hidden=null;
-		}
-		if($(this).attr("id") === "filter_clear"){ chart.update(); return;}
-
-		var n=parseInt($(this).val())-1;
-		for(x in chart.data.datasets){
-			if(n!=x){
-				var a=chart.getDatasetMeta(x);
-				a.hidden=null===a.hidden ?! chart.data.datasets[x].hidden : null;
-			}
-
-		}
-		chart.update();
-		return false;
-
-	});
-
-
-}
 function init_pages_thumbs(){
 	// NB This paging is managed on django until we can do so on transkribus rest
 	// would be great to manage page size and pages with datatable... but this is not a datatable....
@@ -537,7 +232,8 @@ function get_thumbs(start,length){
 	var ids = parse_path();	
 //	var url = static_url+"/dashboard/table_ajax/pages/"+ids['collId']+'/'+ids['docId'];
 //	var url = make_url("/table_ajax/pages/"+ids['collId']+'/'+ids['docId']);
-	var url = serverbase+"/utils/table_ajax/pages/"+ids['collId']+'/'+ids['docId'];
+//	var url = serverbase+"/utils/table_ajax/pages/"+ids['collId']+'/'+ids['docId'];
+	var url = make_url("/utils/table_ajax/pages/"+ids['collId']+'/'+ids['docId']);
 
 	console.log("get_thumbs, URL: ",url);
 
@@ -655,94 +351,3 @@ glyph_opts = {
       loading: "glyphicon glyphicon-refresh"
     }
   };
-/* The fancytree rendering */
-$(document).ready(function(){
-
-    	$(".menu-toggle").click(function(e) {
-
-	   if($(this).attr("id") === "menu-toggle-in"){
-		$(".read_nav_out").show();
-		$(".read_nav_in").hide();
-		localStorage.setItem("sidebar_state", "in");
-	   }else{
-		$(".read_nav_out").hide();
-		$(".read_nav_in").show();
-		localStorage.setItem("sidebar_state", "out");
-	   }
-           $("#wrapper").toggleClass("toggled");
-           e.preventDefault();
-
-    	});
-	if(typeof t_data === "undefined") t_data = [];
-//	$(".pager").hide();
-	$("#collections_tree").fancytree({
-		  source: t_data,  //source in from transkribus
-		  extensions: ["glyph", "wide"],
-		  glyph: glyph_opts,
-		  checkbox: true,
-		  selectMode: 2,
-		  loadChildren: function(event, data) {
-			console.log("Loaded children...",data);
-		  },
-		  activate: function(event, data){
-			var node = data.node,
-			orgEvent = data.originalEvent;
-			console.log("active node: ",data);
-			$(".pager").show();
-			$(".documents_intro").hide();
-			if(node.isFolder()){ //we have a document
-				//put the page thumbs back to small
-				$(".page_thumb").show().removeClass("col-md-12").addClass("col-md-3");
-				//hide other documents	
-				$("#doc_"+node.data.docId).siblings(".document_thumbs").hide();
-				//show active document
-				$("#doc_"+node.data.docId).show().find("span.page_title").html("");
-
-			}else{ //we have a page
-				//first show the doc div that this page is in
-				$("#page_"+node.key).parents(".document_thumbs").siblings().hide();
-				$("#page_"+node.key).parents(".document_thumbs").show();
-
-				//hide all the other pages
-				$("#page_"+node.key).siblings(".page_thumb").hide();
-				//show active page and make it full size (of panel)
-				$("#page_"+node.key).show().removeClass("col-md-3", function(){
-					$(this).addClass("col-md-12");
-				});
-
-				//update the page_title span in the document h4
-				var page_link = "/library/page/"+node.data.collId+"/"+node.data.docId+"/"+node.data.pageNr;
-
-				$("#page_"+node.key).parents(".document_thumbs").find("span.page_title > a").html(node.title).attr("href",page_link);
-			}
-		   },
-	}); //endof fancytree
-
-	/*panel expand/shrink (possibly not used*/
-	$(".panel-expand").click(function(){
-		var button = this;
-//		$(this).parents(".expandable").removeClass("col-md-4", function(){
-	
-		var class_to_remove = ($(this).parents(".expandable").attr("class").match(/(^|\s)col-md-\S+/g) || []).join(" ");
-		$(this).parents(".expandable").removeClass(class_to_remove, function(){
-
-			$(this).addClass("col-md-12");
-			$(button).hide(function(){
-				$(button).siblings(".panel-shrink").attr("data-return-class",class_to_remove);
-				$(button).siblings(".panel-shrink").show()
-			});
-
-		});
-	});
-	$(".panel-shrink").click(function(){
-		var button = this;
-		var class_to_reinstate = $(button).attr("data-return-class");
-		$(this).parents(".expandable").addClass(class_to_reinstate, function(){
-			$(this).removeClass("col-md-12");
-			$(button).hide(function(){$(button).siblings(".panel-expand").show()});
-		});
-	});
-
-});
-
-
