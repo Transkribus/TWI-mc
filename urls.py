776a5e6fdbad8e5e10f569241c8cf50979cdc74f
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2017-03-28 00:24:12 +0100

    use library/collId/docId/etc/etc

--- urls.py
+++ urls.py
@@ -4,17 +4,33 @@ from . import views
 
 urlpatterns = [
 
+#BIG CHANGE: we stop using library/something/number urls
+# Start using /library/number the level is implicit in the number of ids supplied
+
+#collections and index become one
     url(r'^$', views.collections, name='index'),
-    url(r'^collections$', views.collections, name='collections'),
-    url(r'^collection/([\-0-9]+)$', views.collection, name='collection'),
-    url(r'^collection_noaccess/([\-0-9]+)$', views.collection_noaccess, name='collection_noaccess'),
-    url(r'^document/([\-0-9]+)/([0-9]+)$', views.document, name='document'),
-    url(r'^page/([\-0-9]+)/([0-9]+)/([0-9]+)$', views.page, name='page'),
-    url(r'^transcript/([\-0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)$', views.transcript, name='transcript'),
-    url(r'^region/([\-0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)/([\w-]+)$', views.region, name='region'), #TODO improve regionId regex?
-    url(r'^line/([\-0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)/([\w-]+)/([\w-]+)$', views.line, name='line'), #TODO as above...
-    url(r'^word/([\-0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)/([\w-]+)/([\w-]+)/([\w-]+)$', views.word, name='word'), #TODO as above...
-    url(r'^rand/([\-0-9]+)/(word|line|region|transcript|document)$', views.rand, name='rand'),
+#    url(r'^collections$', views.collections, name='collections'),
+    url(r'^([\-0-9]+)$', views.collection, name='collection'),
+#    url(r'^collection_noaccess/([\-0-9]+)$', views.collection_noaccess, name='collection_noaccess'),
+    url(r'^([\-0-9]+)/([0-9]+)$', views.document, name='document'),
+    url(r'^([\-0-9]+)/([0-9]+)/([0-9]+)$', views.page, name='page'),
+    url(r'^([\-0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)$', views.transcript, name='transcript'),
+    url(r'^([\-0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)/([\w-]+)$', views.region, name='region'), #TODO improve regionId regex?
+    url(r'^([\-0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)/([\w-]+)/([\w-]+)$', views.line, name='line'), #TODO as above...
+    url(r'^([\-0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)/([\w-]+)/([\w-]+)/([\w-]+)$', views.word, name='word'), #TODO as above...
+    url(r'^([\-0-9]+)/(word|line|region|transcript|document)$', views.rand, name='rand'),
+
+#    url(r'^$', views.collections, name='index'),
+#    url(r'^collections$', views.collections, name='collections'),
+#    url(r'^collection/([\-0-9]+)$', views.collection, name='collection'),
+#    url(r'^collection_noaccess/([\-0-9]+)$', views.collection_noaccess, name='collection_noaccess'),
+#    url(r'^document/([\-0-9]+)/([0-9]+)$', views.document, name='document'),
+#    url(r'^page/([\-0-9]+)/([0-9]+)/([0-9]+)$', views.page, name='page'),
+#    url(r'^transcript/([\-0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)$', views.transcript, name='transcript'),
+#    url(r'^region/([\-0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)/([\w-]+)$', views.region, name='region'), #TODO improve regionId regex?
+#    url(r'^line/([\-0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)/([\w-]+)/([\w-]+)$', views.line, name='line'), #TODO as above...
+#    url(r'^word/([\-0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)/([\w-]+)/([\w-]+)/([\w-]+)$', views.word, name='word'), #TODO as above...
+#    url(r'^rand/([\-0-9]+)/(word|line|region|transcript|document)$', views.rand, name='rand'),
 
     url(r'^error$', views.error, name='error'),
 
