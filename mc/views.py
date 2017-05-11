a8cb69f4363e117f79bff08ee3d9bef01005a96d
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2017-05-11 12:57:35 +0100

    redir to library

--- mc/views.py
+++ mc/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 def index(request):
-    return HttpResponseRedirect('dashboard/')
+    return HttpResponseRedirect('library/')
 
 def about(request):
     return render(request, 'pages/about.html')
