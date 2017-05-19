051c055b458f538e4a46bbcb9dbb763267bd46ff
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2017-05-19 09:39:45 +0100

    use new transkribus session ci

--- views.py
+++ views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 
-from apps.utils.decorators import t_login_required
-
-
 # Create your views here.
 
-#@t_login_required
 def index(request):
     return render(request, 'search/homepage.html' )
