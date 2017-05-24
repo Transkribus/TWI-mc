2186d6634a0df27a3b3ed4309b82353de19f24b6
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2017-05-24 20:10:17 +0100

    nav_up exception for when in user view

--- contexts.py
+++ contexts.py
@@ -28,6 +28,8 @@ def nav_up(request):
         return {'nav_up' : re.sub(r'\/\d+\/\d+$',"",request.path)}
     if re.search(r'edit\/correct\/\d+\/\d+\/\d+$', request.path):
         return {'nav_up' : re.sub(r'edit\/correct(\/\d+\/\d+)\/\d+$',r'library\1',request.path)}
+    if re.search(r'dashboard\/\d+\/u\/.+$', request.path):
+        return {'nav_up' : re.sub(r'\/u\/.+$',"",request.path)}
 
     return {'nav_up': re.sub(r'\/[^\/]+$',"",request.path)}
 
