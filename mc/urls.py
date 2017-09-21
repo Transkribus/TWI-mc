0f49aa905fafc89ee6624b0ff0fb06c2dd976531
Kansallisarkisto <matti.jokinen@narc.fi>
2017-09-21 10:17:48 +0300

    Submodule update

--- mc/urls.py
+++ mc/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     url(r'^dashboard/', include('apps.dashboard.urls', app_name='dashboard', namespace='dashboard')),
     url(r'^library/', include('apps.library.urls', app_name='library', namespace='library')),
     url(r'^search/', include('apps.search.urls', app_name='search', namespace='search')),
-    url(r'^view/', include('apps.edit.urls', app_name='edit', namespace='view')),
+    url(r'^view/', include('apps.edit.urls', app_name='edit', namespace='edit')),
     url(r'^edit/', include('apps.edit.urls', app_name='edit', namespace='edit')),
     url(r'^utils/', include('apps.utils.urls', app_name='utils', namespace='utils')),
 
