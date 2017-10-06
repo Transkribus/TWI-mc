9d281752b111c4af213192ba5dd49a24be93809b
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2017-10-06 16:57:46 +0100

    add a logout redir url

--- mc/settings/production.py
+++ mc/settings/production.py
@@ -243,6 +243,9 @@ LOGIN_URL = 'login'
 #Where to on login if no next param available?
 LOGIN_REDIRECT_URL = '/library'
 
+#Where to on logout if no next param available?
+LOGOUT_REDIRECT_URL = '/library'
+
 #Default for size of paged data
 PAGE_SIZE_DEFAULT = 5
 
