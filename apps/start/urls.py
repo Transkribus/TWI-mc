86e7c53a96054c5e919f18e1098f7e4a201fb3ae
albertgreinoecker <albert.greinoecker@gmail.com>
2018-02-17 11:19:22 +0100

    added login, logout, change lang

--- apps/start/urls.py
+++ apps/start/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import include, url
 
-from . import views
+from . import views as v
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^start_admin', views.admin, name='fh_admin'),
+    url(r'^$', v.index, name='index'),
+    url(r'^index$', v.index, name='index'),
+    url(r'^start_admin', v.admin, name='fh_admin'),
+    url(r'^store_admin', v.store_admin, name='store_admin'),
+    #url(r'^script', v.script, name='script'),
+    url(r'^login$', v.login_process, name='login_process'),
+    url(r'^logout$', v.logout_process, name='logout_process'),
+    url(r'^change_lang', v.change_lang , name='change_lang')
 ]
