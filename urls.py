f35538c2cf9974156dfa69cc09e732bb9e87b1e3
bejohu <bejohu@yandex.com>
2018-03-05 17:54:37 +0100

    removed login_required decorator

--- urls.py
+++ urls.py
@@ -1,24 +1,18 @@
-from django.conf.urls import include, url
-from django.contrib.auth.decorators import login_required
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
 
-#BIG CHANGE: we stop using library/something/number urls
-# Start using /library/number the level is implicit in the number of ids supplied
-
-#collections and index become one
-
-    url(r'^$', login_required(views.CollectionListView.as_view()), name='index'),
-    url(r'^collections/$', login_required(views.CollectionListView.as_view()), name='collection-list'),
+    url(r'^$', views.CollectionListView.as_view(), name='index'),
+    url(r'^collections/$', views.CollectionListView.as_view(), name='collection-list'),
     url(r'^collections/(?P<col_id>\d+)/$', views.collection_detail, name='collection-detail'),
-
-    url(r'^collections/(?P<col_id>\d+)/documents/$', login_required(views.DocumentListView.as_view()), name='document-list'),
+    url(r'^collections/(?P<col_id>\d+)/documents/$', views.DocumentListView.as_view(), name='document-list'),
     url(r'^collections/(?P<col_id>\d+)/documents/(?P<doc_id>\d+)/$', views.document_detail, name='document-detail'),
+    url(r'^(?P<col_id>\d+)/$', views.DocumentListView.as_view(), name='document-list--short'),
 
 #    url(r'^collections$', views.collections, name='collections'),
-    url(r'^([\-0-9]+)$', views.collection, name='collection'),
+    # url(r'^([\-0-9]+)$', views.collection, name='collection'),
 #    url(r'^([\-0-9]+)/([0-9]+)$', views.document, name='document'),
     url(r'^([\-0-9]+)/([0-9]+)/([0-9]+)$', views.document_page, name='document_page'),
     url(r'^([\-0-9]+)/([0-9]+)$', views.document_page, name='document_page'),
