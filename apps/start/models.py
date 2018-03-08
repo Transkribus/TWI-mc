8801a3e7837b696cb4dbe57e2e935c845ec22a2d
albert <albert@jaco>
2018-03-08 21:08:00 +0100

    added Blog-Entries

--- apps/start/models.py
+++ apps/start/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 
 
-#Store dynamic content of the page
+class Blog(models.Model):
+  a_key = models.IntegerField()
+  language = models.CharField("Title", max_length=2)
+  title = models.CharField("Title", max_length=200)
+  content = models.TextField()
+  changed = models.DateField(auto_now_add=True)
+  
+  class Meta:
+      unique_together = (("a_key", "language"),)
+       
+  def __str__(self):
+      return str(self.title) + ":"  + str(self.content) + "(" + str(self.changed) + ")\n" 
+    
+
+# TODO add image per article?
+# Storage of the top carousel articles
 class Article (models.Model):
   a_key = models.IntegerField()
   language = models.CharField("Title", max_length=2)
