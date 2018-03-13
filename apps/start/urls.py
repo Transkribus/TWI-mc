f23a4e2f11e9bfef3065403b2a4b86aef57d9778
albert <albert@jaco>
2018-03-13 20:57:38 +0100

    added store_admin_blog

--- apps/start/urls.py
+++ apps/start/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^index$', v.index, name='index'),
     url(r'^start_admin', v.admin, name='fh_admin'),
     url(r'^store_admin', v.store_admin, name='store_admin'),
+    url(r'^store_admin_blog', v.store_admin_blog, name='store_admin_blog'),
     #url(r'^script', v.script, name='script'),
     url(r'^login$', v.login_process, name='login_process'),
     url(r'^logout$', v.logout_process, name='logout_process'),
