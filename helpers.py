2142408c9af8df5ee6800e4afd047e0b546a7ba8
bejohu <bejohu@yandex.com>
2018-03-14 18:20:14 +0100

    get url from settings

--- helpers.py
+++ helpers.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 def get_thumb_url(image_id):
-    url = 'https://dbis-thure.uibk.ac.at/f/Get'
+    url = settings.IMAGE_BASE_URL
     return '%s(url)s?id=%(id)&fileType=thumb' % {'url': url.lstrip('/'), 'id': image_id} 
