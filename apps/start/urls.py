ed42ed6f18925eab6fa954ab92f6a584b97ffbe4
albert <albert@jaco>
2018-03-15 22:13:41 +0100

    added urls for the blog editor

--- apps/start/urls.py
+++ apps/start/urls.py
@@ -6,13 +6,14 @@ urlpatterns = [
     url(r'^$', v.index, name='index'),
     url(r'^index$', v.index, name='index'),
     url(r'^start_admin', v.admin, name='fh_admin'),
-    url(r'^store_admin', v.store_admin, name='store_admin'),
+    #url(r'^store_admin', v.store_admin, name='store_admin'),
     url(r'^store_admin_blog', v.store_admin_blog, name='store_admin_blog'),
     #url(r'^script', v.script, name='script'),
     url(r'^login$', v.login_process, name='login_process'),
     url(r'^logout$', v.logout_process, name='logout_process'),
     url(r'^change_lang', v.change_lang , name='change_lang'),
     url(r'^contact', v.contact , name='contact'),
-    url(r'^register_process', v.contact , name='contact'),
-    
+    url(r'^register_process', v.contact , name='register_process'),
+    url(r'^upload_blog_img', v.upload_blog_img, name='upload_blog_img'),
+    url(r'^change_admin_blog', v.change_admin_blog, name='change_admin_blog')
 ]
