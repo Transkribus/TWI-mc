87028b7111a8c37b05412eaa99c2c7c244c015df
albert <albert@jaco>
2018-03-15 22:14:54 +0100

    added comments

--- apps/start/models.py
+++ apps/start/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 
 
+# Main Class for blogs, linked to the entries, which do contain real content
 class Blog(models.Model):
-  a_key = models.IntegerField()
-  language = models.CharField("Title", max_length=2)
-  title = models.CharField("Title", max_length=200)
-  content = models.TextField()
   changed = models.DateField(auto_now_add=True)
   image = models.FilePathField(max_length=512, null=True)
-  
-  class Meta:
-      unique_together = (("a_key", "language"),)
-       
-  def __str__(self):
-      return str(self.title) + ":"  + str(self.content) + "(" + str(self.changed) + ")\n" 
     
+class BlogEntry(models.Model):
+  title = models.CharField("Title", max_length=200)
+  subtitle = models.CharField("Title", max_length=512, null=True)
+  content = models.TextField()
+  lang = models.CharField(max_length=2)
+  blog = models.ForeignKey(Blog, on_delete=models.CASCADE, blank=True, null=True)
+
+
+
+#-------------------------------------------------------------------------------
 
 # TODO add image per article?
 # Storage of the top carousel articles
