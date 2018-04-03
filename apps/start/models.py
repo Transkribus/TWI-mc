0f1fe3bca4d7233909d1f3e98b814f2d4f6d9318
albert <albert@jaco>
2018-04-03 11:28:40 +0200

    Added Home Articles

--- apps/start/models.py
+++ apps/start/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
 
+# Storage of the top carousel articles
+class HomeArticle (models.Model):
+    changed = models.DateField(auto_now_add=True)
+    image = models.FilePathField(max_length=512, null=True)
+    
+class HomeArticleEntry (models.Model):
+    lang = models.CharField("Title", max_length=2)
+    title = models.CharField("Title", max_length=200)
+    shortdesc = models.TextField()
+    content = models.TextField()
+    changed = models.DateField(auto_now_add=True)
+    article = models.ForeignKey(HomeArticle, on_delete=models.CASCADE, blank=True, null=True, related_name='article')
+    
 # Main Class for blogs, linked to the entries, which do contain real content
 class Blog(models.Model):
     changed = models.DateField(auto_now_add=True)
@@ -12,7 +25,7 @@ class BlogEntry(models.Model):
     content = models.TextField()
     lang = models.CharField(max_length=2)
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE, blank=True, null=True, related_name='blog')
-
+    changed = models.DateField(auto_now_add=True)
 
 
 class Institution(models.Model):
@@ -22,38 +35,25 @@ class Institution(models.Model):
     loclabel=models.CharField(max_length=30) #the label of the location for displaying on the map
     link = models.CharField(max_length=512, null=False) #the link to the institution
     image = models.FilePathField(max_length=512, null=True)
+    changed = models.DateField(auto_now_add=True)
 
 class InstitutionDescription(models.Model):
     desc = models.TextField() # full description
     lang = models.CharField(max_length=2)
     inst = models.ForeignKey(Institution, on_delete=models.CASCADE, blank=True, null=True)
+    changed = models.DateField(auto_now_add=True)
     
 class InstitutionProject(models.Model):
     inst = models.ForeignKey(Institution, on_delete=models.CASCADE, blank=True, null=True)
+    changed = models.DateField(auto_now_add=True)
     
 class InstitutionProjectEntries(models.Model):
     title = models.CharField("Title", max_length=200)
     desc = models.TextField() # full description
     lang = models.CharField(max_length=2)
     project = models.ForeignKey(InstitutionProject, on_delete=models.CASCADE, blank=True, null=True)
-    
-    
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-
-# TODO add image per article?
-# Storage of the top carousel articles
-class Article (models.Model):
-    a_key = models.IntegerField()
-    language = models.CharField("Title", max_length=2)
-    title = models.CharField("Title", max_length=200)
-    content = models.TextField()
     changed = models.DateField(auto_now_add=True)
     
-    class Meta:
-        unique_together = (("a_key", "language"),)
-       
-    def __str__(self):
-        return str(self.title) + ":"  + str(self.content) + "(" + str(self.changed) + ")\n" 
+    
+
   
