4b1f7fbb34f236fd3cd7960087aa0418bbd7cada
bejohu <bejohu@yandex.com>
2018-04-20 17:49:12 +0200

    increase request timeout

--- services.py
+++ services.py
@@ -243,7 +243,7 @@ class LazyJsonRequest(Request):
 
         params.update(self._params)
 
-        r = requests.request(self._method, self._url, params=params, timeout=1.5, **self._kwargs)
+        r = requests.request(self._method, self._url, params=params, timeout=5, **self._kwargs)
         r.raise_for_status()
 
         self._result = r.json()
