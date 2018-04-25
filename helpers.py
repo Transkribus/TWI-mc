426a5fa04caadf6b664f903d7ce218146cb30648
bejohu <bejohu@yandex.com>
2018-04-25 16:10:38 +0200

    added default image server url

--- helpers.py
+++ helpers.py
@@ -1,5 +1,5 @@
 from django.conf import settings
 
 def get_thumb_url(image_id):
-    url = settings.IMAGE_BASE_URL
+    url = getattr(settings, 'IMAGE_BASE_URL', 'https://dbis-thure.uibk.ac.at/f/Get')
     return '%(url)s?id=%(id)s&fileType=thumb' % {'url': url.lstrip('/'), 'id': image_id} 
