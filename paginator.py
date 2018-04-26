14d88d92a318fdb77f54da0cb917c26f8d399fbf
bejohu <bejohu@yandex.com>
2018-04-26 11:49:28 +0200

    fixed pagination range issue, quick link to first and lange pages

--- paginator.py
+++ paginator.py
@@ -4,12 +4,34 @@ class Page(paginator.Page):
 
     page_range_width = 5
 
+    @property
     def page_range(self):
+
+        # NOTE: usually, there is no page_range on Page class
+
         width = self.page_range_width
-        start = max(1, self.number - int(width / 2))
         num_pages = self.paginator.num_pages
-        stop = start + self.page_range_width
-        return range(start, stop)
+
+        start = max(1, self.number - int(width / 2))
+        length = min(width, num_pages)
+
+        delta = (start + length - 1) - num_pages
+
+        if delta > 0:
+           start -= delta
+
+        return range(start, start + length)
+
+    @property
+    def is_first_in_range(self):
+        # NOTE: this is also non-standard
+        return min(self.page_range) == 1
+
+    @property
+    def is_last_in_range(self):
+        # NOTE: non-standard method
+        return max(self.page_range) == self.paginator.num_pages
+
 
 class Paginator(paginator.Paginator):
 
