f2a9af2ccd7d0dd560b55e433cc2d70a917ef86f
bejohu <bejohu@yandex.com>
2018-05-02 21:33:01 +0200

    disable old tsdata model

--- models.py
+++ models.py
@@ -10,12 +10,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from picklefield.fields import PickledObjectField
 
-class TSData(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    userId = models.CharField(max_length=100, default=0)
-    gender = models.CharField(max_length=100)
-    affiliation = models.CharField(max_length=100)
-    t = PickledObjectField(null=True)
-    sessionId = models.CharField(max_length=100,null=True,blank=True)
+from django.conf import settings
+# class TSData(models.Model):
+    # user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # userId = models.CharField(max_length=100, default=0)
+    # gender = models.CharField(max_length=100)
+    # affiliation = models.CharField(max_length=100)
+    # t = PickledObjectField(null=True)
+    # sessionId = models.CharField(max_length=100,null=True,blank=True)
     #Add fields for any user data local to app here
     #thingthatappneedstostoreperuser = models.WhateverField();
