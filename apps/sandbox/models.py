c9cb1c2349af79ee65a8e22d5833b4ca5dc50d7f
Rory McNicholl <Rory.McNicholl@london.ac.uk>
2018-05-11 11:42:56 +0100

    remove apps/sanbox content from twi-mc

--- apps/sandbox/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth import get_user_model
-
-class Config(models.Model):
-    owner = models.OneToOneField(get_user_model())
-    # all the fields ...
