78a507bb8028472671e6b6db3a412cf0585c2198
albertgreinoecker <albert.greinoecker@gmail.com>
2018-05-17 11:59:56 +0200

    show info for first institution by default

--- apps/start/static/start/js/main.js
+++ apps/start/static/start/js/main.js
@@ -1,10 +1,23 @@
 $(document).ready(function()
 {
-    console.log("ready");
 
     $("#institution-select").selectBoxIt({
           theme: "bootstrap",
           showEffect: "slideDown",
           autoWidth: true,      
      });
+     
+     
+     var id = $("#institution-select option:first-child").val()
+     change_inst_select(id);
 });
+
+
+
+function change_inst_select(id)
+{
+    ins = inst[id]
+    map.setCenter({lat:ins.lat, lng:ins.lng});
+    $("#inst_desc").html(ins.desc);
+    
+}
