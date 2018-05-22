a719a60a72e35fe850fb7ddd5d3ecb792e361797
bejohu <bejohu@users.noreply.github.com>
2018-05-22 10:44:18 +0200

    Added config for database router

--- mc/settings/production.py
+++ mc/settings/production.py
@@ -148,6 +148,7 @@ DATABASES = {
     }
 }
 
+DATABASE_ROUTERS = ['apps.library.routers.TranskribusRouter']
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
