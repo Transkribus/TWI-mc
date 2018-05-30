8262dc8bcab1c176bdbd74c47e38fdf892b0b70a
bejohu <bejohu@yandex.com>
2018-05-30 15:46:14 +0200

    fixed user admin

--- mc/admin.py
+++ mc/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 from apps.transkribus.models import User
+from django.contrib.auth.admin import GroupAdmin, UserAdmin
 
-admin.register(User)(admin.ModelAdmin)
+admin.site.register(User, UserAdmin)
