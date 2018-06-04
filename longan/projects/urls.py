1c0102fb0eb20c59bdb2f4942f1f0a205d998a27
bejohu <bejohu@yandex.com>
2018-06-04 09:40:26 +0200

    decreased cache timeout for projects view

--- longan/projects/urls.py
+++ longan/projects/urls.py
@@ -6,5 +6,5 @@ from transkribus.decorators import login_required
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<slug_or_id>[\w-]+)/$', login_required(cache_page(60 * 15)(views.project_detail)), name='project-detail')
+    url(r'^(?P<slug_or_id>[\w-]+)/$', login_required(cache_page(60 * 5)(views.project_detail)), name='project-detail')
 ]
