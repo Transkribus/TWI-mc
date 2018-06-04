dac214741c81477bb5e73fbefaf340e00c245b22
bejohu <bejohu@yandex.com>
2018-06-04 09:45:47 +0200

    set sqlite timeout

--- longan/settings/production.py
+++ longan/settings/production.py
@@ -12,6 +12,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(PROJECT_ROOT, 'tmp/db.sqlite3'),
+        'OPTIONS': {
+            'TIMEOUT': 60
+        }
     }
 }
 
