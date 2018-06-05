821ee58cb850bd717306822b16dd80561cd67a6c
bejohu <bejohu@yandex.com>
2018-06-05 12:27:26 -0400

    changed python verison

--- longan/wsgi.py
+++ longan/wsgi.py
@@ -4,13 +4,13 @@ WSGI config for mc project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "longan.settings")
 
 application = get_wsgi_application()
