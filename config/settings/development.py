925fcfed029fae667b139a1e9eae1dac53c4413f
bejohu <bejohu@yandex.com>
2018-06-06 11:36:52 +0200

    set loglevel to info for dev config

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,8 +1,7 @@
 from .base import *
+from .secret import *
 
 BASE_URL = '/readTest'
-
-SECRET_KEY = 'enter-your-secret-here'
 
 DEBUG = False
 
@@ -12,8 +11,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(PROJECT_ROOT, 'tmp/db.sqlite3'),
+        'OPTIONS': {
+            'timeout': 60
+        }
     }
 }
+
+DATABASE_ROUTERS = ['library.routers.TranskribusRouter']
 
 STATIC_URL = '/'.join([BASE_URL, STATIC_URL])
 MEDIA_URL = '/'.join([BASE_URL, MEDIA_URL])
@@ -22,7 +26,23 @@ LOGIN_REDIRECT_URL = '/'.join([BASE_URL, LOGIN_REDIRECT_URL])
 LOGOUT_REDIRECT_URL = '/'.join([BASE_URL, LOGOUT_REDIRECT_URL])
 
 LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+     'formatters': {
+        'request': {
+            'format':'[%(asctime)s] - %(levelname)s - %(module)s : %(message)s',
+            'datefmt' : '%d/%b/%Y %H:%M:%S'
+        },
+    },
     'handlers': {
+        'logfile': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(PROJECT_ROOT, 'logs', 'errors'),
+            'maxBytes': 50000,
+            'backupCount': 2,
+            'formatter': 'request',
+        },
         'console': {
             'class': 'logging.StreamHandler',
             'formatter' : 'request',
@@ -30,8 +50,8 @@ LOGGING = {
     },
     'loggers': {
         'django': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+            'handlers': ['console', 'logfile'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
         },
     },
 }
