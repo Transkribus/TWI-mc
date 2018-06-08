8e6500c1fced43d431273037c5bfbc698abf1371
bejohu <bejohu@yandex.com>
2018-06-08 16:32:24 +0200

    added more compat functions

--- longan/compat/utils.py
+++ longan/compat/utils.py
@@ -1,2 +1,25 @@
 def t_log(*args, **kwargs): pass
 def t_gen_request_id(*args, **kwargs): pass
+
+###########################################################
+# crop(list coords, boolean offset=None)
+# turn polygon coords into rectangle coords or xywh if offset flag set
+def crop(coords, offset=None):
+   # coords = region.get("Coords").get("@points")
+    points = coords.split()
+    xmin=ymin=99999999 #TODO durh...
+    xmax=ymax=0
+    points = [list(map(int, point.split(','))) for point in points]
+    #boo two loops! but I like this one above here...
+    #TODO woops... I actually need this to give the x-off y-off width and height...
+    for point in points:
+        if point[1] > ymax : ymax=point[1]
+        if point[1] < ymin : ymin=point[1]
+        if point[0] > xmax : xmax=point[0]
+        if point[0] < xmin : xmin=point[0]
+    if offset:
+        crop = {'x':xmin, 'y':ymin, 'w':(xmax-xmin), 'h': (ymax-ymin)}
+    else:
+        crop = {'tl':[xmin,ymin], 'tr': [xmax,ymin], 'br': [xmax,ymax], 'bl': [xmin,ymax]}
+
+    return crop
