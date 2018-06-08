46f3fca2d385baa0095656588bebc0db156ac572
bejohu <bejohu@yandex.com>
2018-06-08 17:56:12 +0200

    remove empty line

--- config/wsgi.py
+++ config/wsgi.py
@@ -14,7 +14,6 @@ from django.core.wsgi import get_wsgi_application
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
 application = get_wsgi_application()
