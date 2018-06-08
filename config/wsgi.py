781ff4f88e449a643a409d2d53a58d37f988f9a3
bejohu <bejohu@yandex.com>
2018-06-08 13:30:39 +0200

    fix import order

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,10 +10,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 import sys
 
+from django.core.wsgi import get_wsgi_application
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
