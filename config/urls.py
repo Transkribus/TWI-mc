33f5fa424e1be3e76c06fd26dfa17d3b36611338
bejohu <bejohu@yandex.com>
2018-06-08 18:31:19 +0200

    change name to home

--- config/urls.py
+++ config/urls.py
@@ -21,8 +21,8 @@ from transkribus.views import LoginWithCookie
 
 urlpatterns = [
 
-    url(r'^$', include('library.urls', namespace='home')),
-    url(r'^home/', include('home.urls', namespace='home')),
+    url(r'^', include('home.urls', namespace='home')),
+    # url(r'^home/', include('home.urls', namespace='home')),
     url(r'^sandbox/', include('sandbox.urls', namespace='sandbox')),
 
     url(r'^library/', include('library.urls', namespace='library')),
