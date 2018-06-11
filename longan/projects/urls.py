4888e2f6d634b066895157936784edb58aa483fc
bejohu <bejohu@yandex.com>
2018-06-11 17:05:45 +0200

    allow ids as well as slugs

--- longan/projects/urls.py
+++ longan/projects/urls.py
@@ -8,6 +8,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.ProjectListView.as_view(), name='project-list'),
     # url(r'^$', cache_page(3600 * 24)(views.ProjectListView.as_view()), name='project-list'),
-    url(r'^(?P<slug_or_id>[\w-]+)/$', login_required(cache_page(60 * 5)(views.project_detail)), name='project-detail'),
+    url(r'^(?P<slug_or_id>([\w-]+|\d+))/$', login_required(cache_page(60 * 5)(views.project_detail)), name='project-detail'),
     url(r'^(?P<id>\d+)/subscribe/$', login_required(views.subscribe_view), name='subscribe')
 ]
